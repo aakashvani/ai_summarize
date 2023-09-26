f5d77360afc90ef6abb143c109b3b1ab63c9a11d
Aakash Kumar <95979529+aakashvani@users.noreply.github.com>
2023-09-26 17:18:13 +0000

    installed @reduxjs/toolkit and setUp

--- src/component/Demo.jsx
+++ src/component/Demo.jsx
@@ -1,16 +1,31 @@
 import { useState, useEffect } from "react";
 
 const Demo = () => {
+  const [article, setArtical] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
           <img src="" alt="" />
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) =>
+              setArtical({
+                article: { ...article, url: e.target.value },
+              })
+            }
             required
             className="url_input peer "
           />
